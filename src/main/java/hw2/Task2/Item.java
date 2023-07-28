b9fffc49726a5dbc8518ef412a81312ac66ab736
Ivan Rizhov <97846647+kabrakis@users.noreply.github.com>
2023-07-28 19:05:16 +0300

    Update Item.java

--- src/main/java/hw2/Task2/Item.java
+++ src/main/java/hw2/Task2/Item.java
@@ -1,2 +1,37 @@
-package hw2.Task2;public class Item {
+package hw2.Task2;
+
+public class Item {
+    public String name;
+    public int iD;
+    private boolean inStock;
+
+    public int order;
+
+    public Item(String name, int iD, boolean inStock) {
+        this.name = name;
+        this.iD = iD;
+        this.inStock = inStock;
+        int order;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getiD() {
+        return iD;
+    }
+
+    public void setiD(int iD) {
+        this.iD = iD;
+    }
+
+    @Override
+    public String toString() {
+        return "{" + "name='" + name + '\'' + ", id=" + iD + "Наличие на складе" + inStock + '}';
+    }
 }
