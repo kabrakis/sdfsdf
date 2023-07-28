cb56ac0eeb4df5ec830776785e853288ced96957
Ivan Rizhov <97846647+kabrakis@users.noreply.github.com>
2023-07-28 19:06:27 +0300

    Update Order.java

--- src/main/java/hw2/Task2/Order.java
+++ src/main/java/hw2/Task2/Order.java
@@ -1,2 +1,29 @@
-package hw2.Task2;public class Order {
+package hw2.Task2;
+
+
+public class Order {
+    public int number;
+    public String item;
+    public int iD;
+
+
+    public Order(int number) {
+        this.number = number;
+        String item;
+        int iD;
+    }
+
+    public int getNumber() {
+        return number;
+    }
+
+    public void setNumber(int number) {
+        this.number = number;
+    }
+
+
+    public void setItem(String item, int iD) {
+        this.item = item;
+        this.iD = iD;
+    }
 }
