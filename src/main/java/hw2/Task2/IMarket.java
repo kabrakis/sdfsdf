3ba2f9070f96b57562d2dfba60d70ecff1b20300
Ivan Rizhov <97846647+kabrakis@users.noreply.github.com>
2023-07-28 19:05:02 +0300

    Update IMarket.java

--- src/main/java/hw2/Task2/IMarket.java
+++ src/main/java/hw2/Task2/IMarket.java
@@ -1,2 +1,13 @@
-package hw2.Task2;public class IMarket {
+package hw2.Task2;
+
+public interface IMarket {
+    void QueueBehaviourIn(String User, int order);//имитируют работу очереди
+
+    void QueueBehaviourOut(String User, int order);
+
+    void MarketBehaviourIn(String User);//добавляют и удаляют людей из очереди
+
+    void MarketBehaviourOut(String User);
+
+    void update();//обновляет состояние магазина путем принятия и отдачи заказов
 }
